43d57fd520eb6931361994f389264f9bc2fbcb72
nisarlala0987-boop <nisarlala0987@gmail.com>
2026-08-18 22:07:53 +0500

    add start script

--- index.js
+++ index.js
@@ -1,23 +1,31 @@
-import express, { Router } from "express";
+import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import mongoose from "mongoose";
 import router from "./product/route.js";
+
 dotenv.config();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use("/",router)
+app.use("/", router);
 
 app.get("/", (req, res) => {
   res.send("Backend is running");
 });
-mongoose.connect(process.env.MONGO_URL)
-.then(() => console.log("mongodb is connected"))
-.catch((err) => console.log(err))
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
-});
+// ========== YAHAN CHANGE KARO ==========
+const PORT = process.env.PORT || 5000;  // Vercel apna PORT deta hai
+
+mongoose.connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("MongoDB is connected");
+    app.listen(PROcess.env.PORT || 5000, () => {   // Vercel ke liye PORT use karo
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
+  .catch((err) => console.log(err));
+
+// ========== CHANGE KHATAM ==========
