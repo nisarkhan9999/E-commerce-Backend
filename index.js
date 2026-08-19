9f6dc39bb5140237cb49a700f5ccd031199edd9f
nisarlala0987-boop <nisarlala0987@gmail.com>
2026-08-19 09:48:51 +0500

    fix the error

--- index.js
+++ index.js
@@ -16,16 +16,13 @@ app.get("/", (req, res) => {
   res.send("Backend is running");
 });
 
-// ========== YAHAN CHANGE KARO ==========
-const PORT = process.env.PORT || 5000;  // Vercel apna PORT deta hai
+const PORT = process.env.PORT || 5000;
 
 mongoose.connect(process.env.MONGO_URL)
   .then(() => {
     console.log("MongoDB is connected");
-    app.listen(PROcess.env.PORT || 5000, () => {   // Vercel ke liye PORT use karo
+    app.listen(PORT, () => {
       console.log(`Server running on port ${PORT}`);
     });
   })
   .catch((err) => console.log(err));
-
-// ========== CHANGE KHATAM ==========
